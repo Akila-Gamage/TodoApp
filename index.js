bf0f1e1044272ce50f29d73c6c62693eae13c230
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-04-27 20:44:56 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
 app.use(express.urlencoded({ extended:true }));
 
 app.use("/static", express.static("public"));
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }) 
+    .then(() => {
+        console.log("Connected to db!");
+        mongodbapp.listen(3000, () => console.log("Server Up and running")); 
+    })
+    .catch((err) => { console.error(err); });
 
 app.set("view engine", "ejs");
 
@@ -15,4 +24,4 @@ app.post('/', (req, res) =>{
     console.log(req.body);
 })
 
-app.listen(3000, () => console.log("Server up and running"));
+// app.listen(3000, () => console.log("Server up and running"));
