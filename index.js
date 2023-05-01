c9e813d0d5cc3ad08ad1a98084c33a1336139d7a
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-05-01 22:31:31 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+const TodoTask = require("./models/TodoTask");
+
 dotenv.config();
 
 app.use(express.urlencoded({ extended:true }));
@@ -10,7 +13,7 @@ app.use("/static", express.static("public"));
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }) 
     .then(() => {
         console.log("Connected to db!");
-        mongodbapp.listen(3000, () => console.log("Server Up and running")); 
+        app.listen(3000, () => console.log("Server Up and running")); 
     })
     .catch((err) => { console.error(err); });
 
@@ -20,8 +23,14 @@ app.get('/', (req, res) => {
     res.render('todo.ejs');
 });
 
-app.post('/', (req, res) =>{
-    console.log(req.body);
-})
+app.post('/',async (req, res) => { const todoTask = new TodoTask({
+    content: req.body.content });
+        try {
+            await todoTask.save();
+            res.redirect("/");
+        } catch (err) {
+            res.send(500, err);
+            res.redirect("/");
+    } });
 
 // app.listen(3000, () => console.log("Server up and running"));
